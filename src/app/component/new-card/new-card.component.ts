70e712aa8a0ade9a35d3401f1949b2464cf37bad
Frederic Nowacki <nowacki.frederic@gmail.com>
2018-04-29 05:23:56 -0400

    Fix on new card by edition

--- src/app/component/new-card/new-card.component.ts
+++ src/app/component/new-card/new-card.component.ts
@@ -54,7 +54,11 @@ export class NewCardComponent implements OnInit {
 
   createCard() {
     this.cardService.updateCard(this.card).subscribe(() => {
+      if (this.edition) {
+        this.router.navigateByUrl('edition/' + this.edition.id);
+      } else {
       this.router.navigateByUrl('card');
+      }
     });
   }
 
